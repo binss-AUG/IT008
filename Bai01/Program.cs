84bca36f61c0a9f3fa5ff698c077f9f2d5065e7f
binss-AUG <ignore6172@gmail.com>
2025-09-28 17:02:29 +0700

    Lab1 - first time ;3

---
+++ Bai01/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Bai01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.Write("Nhập vào kích thước n của mảng: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        InputArr(arr, n);
+
+        PrintList(arr, n);
+
+    }
+    static void PrintList(int[] arr, int n)
+    {
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine("1: Tính tổng các số lẻ trong mảng: "  + OddSum(arr, n));
+        Console.WriteLine("2: Đếm số nguyên tố trong mảng: " + PrimeCount(arr, n));
+        Console.WriteLine("3: Tìm số chính phương nhỏ nhất (nếu không có hàm trả về -1): " + MinSquareNum(arr,n));
+        Console.WriteLine("-------------------------------------------------------------");
+    }
+    static void InputArr(int[] arr, int n)
+    {
+        Console.Write("Sinh các giá trị: ");
+        Random rand = new Random();
+        for(int i = 0;i < n;i++)
+        {
+
+            arr[i] = rand.Next(-100,100);
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+    static int OddSum(int[] arr, int n)
+    {
+        int sum = 0;
+        for(int i = 0;i < n;i++)
+        {
+            if (Math.Abs(arr[i]) % 2 == 1) sum += arr[i];
+        }
+
+        return sum;
+    }
+    static bool isPrime(int n)
+    {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+    static int PrimeCount(int[] arr, int n)
+    {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (isPrime(arr[i]))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    static bool isSquareNum(int n)
+    {
+        if (n < 0) return false;
+        int temp = (int)Math.Sqrt(n);
+        return (Math.Pow(temp, 2) == n);
+    }
+    static int MinSquareNum(int[] arr, int n)
+    {
+        bool used = false;
+        int res = -1;
+
+        for(int i = 0;i < n;i++)
+        {
+            if (isSquareNum(arr[i]))
+            {
+                if (arr[i] < res || res == -1)
+                {
+                    res = arr[i];
+                }     
+            }
+        }
+        return res;
+        
+    }
+}
---
+++ Bai06/Program.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Bai06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.Write("Nhập vào kích thước dòng của mảng: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhập vào kích thước cột của mảng: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhập vào số thứ tự k là dòng cần xóa : ");
+        int k = int.Parse(Console.ReadLine());
+
+        int[,] arr = new int[n,m];
+
+        InputArr(arr, n, m);
+
+        PrintList(arr, n, m, k);
+
+    }
+    static void PrintList(int[,] arr, int n, int m, int k)
+    {
+        if (isEmpty(n, m))
+        {
+            Console.WriteLine("Ma trận rỗng!");
+            return;
+        }
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine("1: Xuất ma trận: ");
+        PrintArr(arr, n, m);
+        Console.WriteLine("2: Giá trị lớn nhất/nhỏ nhất trong ma trận: " + MaxArr(arr, n, m) + "/" + MinArr(arr, n, m));
+        Console.WriteLine("3: Dòng có tổng giá trị lớn nhất là (-1 nếu tồn tại từ 2 dòng có cùng giá trị tổng lớn nhất): " + MaxRow(arr, n, m));
+        Console.WriteLine("4: Tổng các giá trị không phải số nguyên tố: " + notPrimeSum(arr, n, m));
+        Console.WriteLine("5: Xóa dòng thứ k trong ma trận: ");
+        DelRowK(arr, n, m, k);
+        Console.WriteLine("6: Xóa cột đang giữ giá trị lớn nhất: ");
+        DelMaxCol(arr, n, m);
+        Console.WriteLine("-------------------------------------------------------------");
+    }
+    static void PrintArr(int[,] arr, int n, int m)
+    {
+        if (isEmpty(n,m))
+        {
+            Console.WriteLine("Ma trận rỗng!");
+            return;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine();
+    }
+    static void InputArr(int[,] arr, int n, int m)
+    {
+        Console.WriteLine("Sinh các giá trị: ");
+        Random rand = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            //arr[i] = new int[m];
+            for (int j = 0;j < m;j++)
+            {
+                arr[i,j] = rand.Next(-100, 100);
+            }
+        }
+    }
+    static bool isEmpty(int n, int m)
+    {
+        return (m == 0 || n == 0);
+    }
+    static bool isPrime(int n)
+    {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+    
+    static int MaxArr(int[,] arr, int n, int m)
+    {
+        int max = int.MinValue;
+
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                max = Math.Max(max, arr[i,j]); 
+            }    
+        }
+
+        return max;
+    }
+
+    static int MinArr(int[,] arr, int n, int m)
+    {
+        int min = int.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                min = Math.Min(min, arr[i,j]);
+            }
+        }
+
+        return min;
+    }
+
+    static int MaxRow(int[,] arr, int n, int m)
+    {
+        int posrow = 0;
+        bool alive = false;
+        int maxsum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int tempsum = 0;
+            for (int j = 0; j < m; j++)
+            {
+                tempsum += arr[i,j];
+            }
+            if (i == 0)
+            {
+                maxsum = tempsum;
+                continue;
+            }
+            if (tempsum > maxsum)
+            {
+                posrow = i;
+                maxsum = tempsum;
+                alive = false;
+            }
+            else
+            {
+                if (tempsum == maxsum) alive = true;
+            }
+        }
+
+        if (alive) return -1;
+
+        return posrow;
+
+    }
+
+    static int notPrimeSum(int[,] arr, int n, int m)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (!isPrime(Math.Abs(arr[i, j]))) sum += arr[i,j];
+            }
+        }
+
+        return sum;
+    }
+    static void DelRowK(int[,] arr, int n, int m, int k)
+    {
+        if (n <= 0)
+        {
+            Console.WriteLine("Không xóa được, đã đạt kích thước tôi thiểu!");
+            return;
+        }
+        if (k < 0 || n <= k)
+        {
+            Console.WriteLine("Không xóa được, giá trị k nằm ngoài ma trận!");
+            return;
+        }
+        int[,] res = new int[n-1, m];
+        int ires = 0;
+        for(int i = 0;i < n;i++)
+        {
+            int jres = 0;
+            if (i == k) continue;
+            for(int j = 0;j < m;j++)
+            {
+                res[ires, jres++] = arr[i, j];
+            }
+            ires++;
+        }
+        PrintArr(res, n - 1, m);
+    }
+
+    static void DelColK(int[,] arr, int n, int m, int k)
+    {
+        if (m <= 0)
+        {
+            Console.WriteLine("Không xóa được, đã đạt kích thước tôi thiểu!");
+            return;
+        }
+
+        if (k < 0 || m <= k)
+        {
+            Console.WriteLine("Không xóa được, giá trị k nằm ngoài ma trận!");
+            return;
+        }
+        int[,] res = new int[n, m-1];
+        int jres = 0;
+        for (int j = 0; j < m; j++)
+        {
+            int ires = 0;
+            if (j == k) continue;
+            for (int i = 0; i < n; i++)
+            {
+                res[ires++, jres] = arr[i, j];
+            }
+            jres++;
+        }
+
+        PrintArr(res, n, m - 1);
+    }
+
+    static void DelMaxCol(int[,] arr, int n, int m)
+    {
+        int maxarr = MaxArr(arr, n, m);
+        int maxcolpos = -1;
+        for(int j = 0;j < m;j++)
+        {
+            for(int i = 0;i < n;i++)
+            {
+                if (arr[i, j] == maxarr)
+                {
+                    if (maxcolpos != -1)
+                    {
+                        Console.WriteLine("Không xóa được, tồn tại từ 2 cột chứa phần tử lớn nhất!");
+                        return;
+                    }
+
+                    maxcolpos = j;
+                    break;
+                }
+            }
+        }
+        DelColK(arr, n, m, maxcolpos);
+    }
+}
