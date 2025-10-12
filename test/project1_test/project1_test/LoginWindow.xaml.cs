bad0553d022b04c3424df8ab1f737b8c344b8468
binss-AUG <ignore6172@gmail.com>
2025-10-12 16:27:58 +0700

    testgit add test

---
+++ test/project1_test/project1_test/LoginWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Login
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            
+        }
+
+        private void Login_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
