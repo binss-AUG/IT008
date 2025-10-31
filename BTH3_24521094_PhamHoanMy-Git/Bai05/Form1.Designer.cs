4812645f0aa2cb79172ace9860a7a0fcc3be5c26
binss-AUG <ignore6172@gmail.com>
2025-11-01 03:40:07 +0700

    Update Lab3

---
+++ BTH3_24521094_PhamHoanMy-Git/Bai05/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bai05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.Location = new System.Drawing.Point(50, 50);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(80, 20);
+            this.lblNumber1.TabIndex = 1;
+            this.lblNumber1.Text = "Number 1";
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.Location = new System.Drawing.Point(50, 90);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(80, 20);
+            this.lblNumber2.TabIndex = 2;
+            this.lblNumber2.Text = "Number 2";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.Location = new System.Drawing.Point(50, 130);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(80, 20);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "Answer";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(150, 50);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber1.TabIndex = 4;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(150, 90);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber2.TabIndex = 5;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(150, 130);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(100, 22);
+            this.txtAnswer.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(50, 180);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(40, 30);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "+";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(100, 180);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(40, 30);
+            this.btnSubtract.TabIndex = 8;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(150, 180);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(40, 30);
+            this.btnMultiply.TabIndex = 9;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(200, 180);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(40, 30);
+            this.btnDivide.TabIndex = 10;
+            this.btnDivide.Text = "/";
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnDivide);
+            this.Name = "Form1";
+            this.Text = "Calculator beta 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtNumber1;
+        private TextBox txtNumber2;
+        private TextBox txtAnswer;
+        private Label lblNumber1;
+        private Label lblNumber2;
+        private Label lblAnswer;
+        private Button btnAdd;
+        private Button btnSubtract;
+        private Button btnMultiply;
+        private Button btnDivide;
+    }
+}
+
---
+++ BTH3_24521094_PhamHoanMy-Git/Bai09/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai09
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> students;
+
+        public Form1()
+        {
+            InitializeComponent();
+            students = new List<Student>();
+            InitializeCourses();
+            AttachEventHandlers();
+        }
+
+        private void InitializeCourses()
+        {
+            lstAvailableCourses.Items.AddRange(new string[] {
+                "Cơ Sở Dữ Liệu",
+                "Cơ Sở DL NC",
+                "PTTK Hệ Thống thông tin"
+            });
+        }
+
+        private void AttachEventHandlers()
+        {
+            btnSave.Click += SaveClick;
+            btnClear.Click += ClearClick;
+            btnAddCourse.Click += AddCourseClick;
+            btnRemoveCourse.Click += RemoveCourseClick;
+            dataGridView1.SelectionChanged += DataGridViewSelectionChanged;
+        }
+
+        private bool isMSSV(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!('0' <= s[i] && s[i] <= '9')) return false;
+            }
+            return true;
+        }
+
+        private bool isFullName(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!('a' <= s[i] && s[i] <= 'z') && !('A' <= s[i] && s[i] <= 'Z') && s[i] != ' ') return false;
+            }
+            return true;
+        }
+
+        private void AddCourseClick(object sender, EventArgs e)
+        {
+            if (lstAvailableCourses.SelectedItem != null)
+            {
+                string selectedCourse = lstAvailableCourses.SelectedItem.ToString();
+                lstSelectedCourses.Items.Add(selectedCourse);
+                lstAvailableCourses.Items.Remove(selectedCourse);
+            }
+        }
+
+        private void RemoveCourseClick(object sender, EventArgs e)
+        {
+            if (lstSelectedCourses.SelectedItem != null)
+            {
+                string selectedCourse = lstSelectedCourses.SelectedItem.ToString();
+                lstAvailableCourses.Items.Add(selectedCourse);
+                lstSelectedCourses.Items.Remove(selectedCourse);
+            }
+        }
+
+        private void SaveClick(object sender, EventArgs e)
+        {
+            if (!isMSSV(txtStudentID.Text) ||
+                !isFullName(txtFullName.Text) ||
+                string.IsNullOrEmpty(cboMajor.Text) ||
+                (!radMale.Checked && !radFemale.Checked))
+            {
+                MessageBox.Show("Vui lòng nhập chính xác thông tin!");
+                return;
+            }
+
+            string studentID = txtStudentID.Text;
+            string fullName = txtFullName.Text;
+            string major = cboMajor.Text;
+            string gender = radMale.Checked ? "Nam" : "Nữ";
+
+            List<string> selectedCourses = new List<string>();
+            foreach (var item in lstSelectedCourses.Items)
+            {
+                selectedCourses.Add(item.ToString());
+            }
+            int courseCount = selectedCourses.Count;
+
+            int existingIndex = FindStudentIndex(studentID);
+
+            if (existingIndex == -1)
+            {
+                Student newStudent = new Student
+                {
+                    StudentID = studentID,
+                    FullName = fullName,
+                    Major = major,
+                    Gender = gender,
+                    CourseCount = courseCount,
+                    Courses = selectedCourses
+                };
+                students.Add(newStudent);
+                RefreshDataGridView();
+                ClearInputs();
+                MessageBox.Show("Thêm mới sinh viên thành công!");
+            }
+            else
+            {
+                students[existingIndex].FullName = fullName;
+                students[existingIndex].Major = major;
+                students[existingIndex].Gender = gender;
+                students[existingIndex].CourseCount = courseCount;
+                students[existingIndex].Courses = selectedCourses;
+                RefreshDataGridView();
+                ClearInputs();
+                MessageBox.Show("Cập nhật sinh viên thành công!");
+            }
+        }
+
+        private void ClearClick(object sender, EventArgs e)
+        {
+            ClearInputs();
+        }
+
+        private void DataGridViewSelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                string studentID = selectedRow.Cells["colMSSV"].Value.ToString();
+
+                int studentIndex = FindStudentIndex(studentID);
+                if (studentIndex != -1)
+                {
+                    Student student = students[studentIndex];
+                    txtStudentID.Text = student.StudentID;
+                    txtFullName.Text = student.FullName;
+                    cboMajor.Text = student.Major;
+
+                    if (student.Gender == "Nam")
+                        radMale.Checked = true;
+                    else
+                        radFemale.Checked = true;
+
+                    lstSelectedCourses.Items.Clear();
+                    foreach (var course in student.Courses)
+                    {
+                        lstSelectedCourses.Items.Add(course);
+                    }
+
+                    lstAvailableCourses.Items.Clear();
+                    InitializeCourses();
+                    foreach (var course in student.Courses)
+                    {
+                        if (lstAvailableCourses.Items.Contains(course))
+                        {
+                            lstAvailableCourses.Items.Remove(course);
+                        }
+                    }
+                }
+            }
+        }
+
+        private int FindStudentIndex(string studentID)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].StudentID == studentID)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void RefreshDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var student in students)
+            {
+                dataGridView1.Rows.Add(student.StudentID, student.FullName, student.Major, student.Gender, student.CourseCount);
+            }
+        }
+
+        private void ClearInputs()
+        {
+            txtStudentID.Clear();
+            txtFullName.Clear();
+            cboMajor.SelectedIndex = -1;
+            radMale.Checked = false;
+            radFemale.Checked = false;
+
+            foreach (var item in lstSelectedCourses.Items)
+            {
+                lstAvailableCourses.Items.Add(item);
+            }
+            lstSelectedCourses.Items.Clear();
+        }
+    }
+
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string FullName { get; set; }
+        public string Major { get; set; }
+        public string Gender { get; set; }
+        public int CourseCount { get; set; }
+        public List<string> Courses { get; set; }
+    }
+}
